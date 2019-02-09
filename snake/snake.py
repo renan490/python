33aeb600ce3afb3dbee4f29a241a62a3ffcdb22a
renan490 <renan490@gmail.com>
2019-02-09 19:03:55 -0300

    Pygame

---
+++ snake/snake.py
@@ -0,0 +1,33 @@
+import pygame, random
+from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Snake')
+
+#SNAKE
+snake = [(200, 200), (210,200), (220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill((255,255,255))
+#</SNAKE>
+
+#APPLE
+apple_pos = (random.randint(0,590), random.randint(0,590))
+apple = pygame.Surface((10,10))
+apple.fill((255,0,0))
+#</APLE>
+my_direction = LEFT
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+    screen.fill((0,0,0))
+    screen.blit(apple, apple_pos)
+    for pos in snake:
+        screen.blit(snake_skin,pos)
+    pygame.display.update()
